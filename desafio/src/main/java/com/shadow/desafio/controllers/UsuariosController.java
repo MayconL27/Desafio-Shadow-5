24f128dc50508c7ffbe191b4e11596f9922d7013
MayconL27 <mayconleandro2008@gmail.com>
2022-12-22 10:15:48 -0300

    Método delete funcionando

--- desafio/src/main/java/com/shadow/desafio/controllers/UsuariosController.java
+++ desafio/src/main/java/com/shadow/desafio/controllers/UsuariosController.java
@@ -10,13 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)/* Permitir acesso a qualquer fonte*/
-@RequestMapping("/usuarios")
 public class UsuariosController {
 
     final UsuariosService usuariosService;
@@ -28,7 +25,7 @@ public class UsuariosController {
         this.usuariosRepository = usuariosRepository;
     }
 
-    @PostMapping
+    @PostMapping(value = "salvar")
     public ResponseEntity<Object> salvarUsuarios(@RequestBody @Valid UsuariosDto usuariosDto){
         var usuarios = new Usuarios();/*Obs: Não tem no java 8*/
         BeanUtils.copyProperties(usuariosDto, usuarios); /*Conversão de DTO para Entity ates de salvar*/
@@ -40,10 +37,29 @@ public class UsuariosController {
         return ResponseEntity.status(HttpStatus.OK).body(UsuariosService.findAll());
     }*/
 
-    @GetMapping
+    @GetMapping(value = "listartodos")
     public ResponseEntity<List<Usuarios>> listarTodosUsuarios(){
         List<Usuarios> usuarios = usuariosRepository.findAll();
         return new ResponseEntity<List<Usuarios>>(usuarios, HttpStatus.OK);
+    }
+
+    @PutMapping
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuarios usuario) {
+/*
+        if (usuario.getId() == null) {
+            return new ResponseEntity<String>("Id não foi informado para atualização.", HttpStatus.OK);
+        }*/
+
+        Usuarios user = usuariosRepository.saveAndFlush(usuario);
+
+        return new ResponseEntity<Usuarios>(user, HttpStatus.OK);
+
+    }
+    @DeleteMapping(value = "delete")
+    public ResponseEntity<String> deleteUsuario(@RequestBody Usuarios usuarios) {
+        usuariosRepository.delete(usuarios);
+        return new ResponseEntity<String>("Usuário deletado com sucesso", HttpStatus.OK);
     }
 
 
