31e7a22dc6fed02684c5c2738c596641412dd3e7
MayconL27 <mayconleandro2008@gmail.com>
2023-01-14 12:43:43 -0300

    Port 8082

--- usuario/src/main/java/com/shadow/desafio/repositories/feign/AuthFeign.java
+++ usuario/src/main/java/com/shadow/desafio/repositories/feign/AuthFeign.java
@@ -5,4 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 @FeignClient(name = "feign", url = "")
 public interface AuthFeign {
 
+/*
+    @GetMapping(value = "/auth/validatetoken")
+    boolean validateToken(@RequestHeader(HttpHeaders.AUTHORIZATION)String token);
+
+    @GetMapping(value = "/auth/typeuser")
+    String getTypeUser(@RequestHeader(HttpHeaders.AUTHORIZATION)String token);
+*/
 }
