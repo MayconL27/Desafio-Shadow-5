dcf9dd6a550e2340d6babef38e99233b376b0ee7
MayconL27 <mayconleandro2008@gmail.com>
2023-01-16 11:12:45 -0300

    Commit final entrega

--- usuario/src/main/java/com/shadow/desafio/repositories/feign/AuthFeign.java
+++ usuario/src/main/java/com/shadow/desafio/repositories/feign/AuthFeign.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.http.HttpHeaders;
 
-@FeignClient(name = "feign", url = "http://localhost:8081") // port microservice
+@FeignClient(name = "feign", url = "http://localhost:8081") // port Desafio 7
 public interface AuthFeign {
     @GetMapping(value = "/auth/validartoken")
     boolean validarToken(@RequestHeader(HttpHeaders.AUTHORIZATION)String token);
@@ -13,11 +13,5 @@ public interface AuthFeign {
     @GetMapping(value = "/auth/tipousuario")
     String getTipoUsuario(@RequestHeader(HttpHeaders.AUTHORIZATION)String token);
 
-/*
-    @GetMapping(value = "/auth/validatetoken")
-    boolean validateToken(@RequestHeader(HttpHeaders.AUTHORIZATION)String token);
 
-    @GetMapping(value = "/auth/typeuser")
-    String getTypeUser(@RequestHeader(HttpHeaders.AUTHORIZATION)String token);
-*/
 }
