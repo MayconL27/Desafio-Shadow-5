3f0614315057ebb7faadde39deaeb0def575fc9f
MayconL27 <mayconleandro2008@gmail.com>
2023-02-10 15:28:00 -0300

    Objetos estão mockando como null

--- usuario/src/test/java/com/shadow/desafio/service/UsuariosServiceTest.java
+++ usuario/src/test/java/com/shadow/desafio/service/UsuariosServiceTest.java
@@ -1,8 +1,8 @@
 package com.shadow.desafio.service;
 
-import com.shadow.desafio.dtos.LoginDto;
 import com.shadow.desafio.entities.Usuarios;
 import com.shadow.desafio.repositories.UsuariosRepository;
+import com.shadow.desafio.service.exceptions.UserNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,49 +12,77 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.Optional;
-import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@AutoConfigureMockMvc
 class UsuariosServiceTest {
 
     public static final String ID = "1";
-    public static final String NAME = "Maycon";
+    public static final String NOME = "Maycon";
     public static final String CPF = "51572343257";
     public static final String EMAIL = "maycon@gmail.com";
     public static final String SENHA = "123456";
-    public static final String TIPOCLIENTE = "ADMIN";
+    public static final String CLIENTE = "ADMIN";
+
     @InjectMocks
     private UsuariosService usuariosService;
     @Mock
-    UsuariosRepository usuariosRepository;
+    private UsuariosRepository usuariosRepository;
+
 
     private Usuarios usuarios;
     private Optional<Usuarios> optionalUsuarios;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this.usuariosService );
-
-        usuarios = new Usuarios("1","Maycon","51572343257","maycon@gmail.com","123456","ADMIN");
-        optionalUsuarios = Optional.of(new Usuarios("1","Maycon","51572343257","maycon@gmail.com","123456","ADMIN"));
-
+        MockitoAnnotations.openMocks(this.usuariosService ); // Iniciar os Mocks.
+        startUser();
     }
 
     @Test
     @DisplayName("Deve retornar um usuario ao buscar por ID")
-    void whenFindByIdThenReturnUserInstance() {
+    void  whenFindByIdThenReturnUserInstance() {
         // Optional<Usuarios> usuarios = usuariosRepository.findById(codigoID);
-        Mockito.when(usuariosRepository.findById((Mockito.anyString()))).thenReturn(optionalUsuarios);
+        Mockito.when(usuariosRepository.findById((Mockito.anyString())))
+                .thenReturn(optionalUsuarios);
 
-        Usuarios response = usuariosService.findById("1");
+        Usuarios response = usuariosService.findById(ID);
 
-        Assertions.assertEquals(Usuarios.class, response.getClass());
+        assertNotNull(response); // Assegura que o response não seja null.
 
+        assertNotNull(usuarios);
+
+        assertEquals(Usuarios.class, response.getClass());
+
+        assertEquals(ID, response.getCodigoID());
+
+    }
+
+    @Test
+    @DisplayName("Quando id não encontrado ou não existe deve retorna uma resposta")
+    void whenFindByIdThenReturnAnObjectNotFoundExcepton() {
+        // Quando chamar findByID chama essa mensagem da exception
+        Mockito.when(usuariosRepository.findById(Mockito.anyString()))
+                        .thenThrow( new UserNotFoundException());
+
+        try {
+            usuariosService.findById(ID);
+        } catch (Exception e) {
+            assertEquals(UserNotFoundException.class, e.getClass());
+            assertEquals("Usuário não encontrado", e.getMessage());
+
+        }
     }
 
     @Test
@@ -64,26 +92,6 @@ class UsuariosServiceTest {
 
     }
 
-    @Test
-    @DisplayName("Login de usuario deve retornar ok")
-    void loginUsuario() {
-
-        //setup
-        //Arrange
-        LoginDto loginDto = new LoginDto();
-        loginDto.getEmail();
-        loginDto.getSenha();
-
-//        Mockito.when(usuariosService.loginUsuario()).thenReturn(Optional.of(usuariosRepository))
-
-        //teste
-        //Action
-        usuariosService.loginUsuario(loginDto);
-
-        //validacoes
-        //Assertions
-
-    }
 
     @Test
     void existsByEmail() {
@@ -99,5 +107,10 @@ class UsuariosServiceTest {
 
     @Test
     void validarToken() {
+    }
+
+    private void startUser() {
+        usuarios = new Usuarios(ID, NOME, CPF, EMAIL, SENHA, CLIENTE);
+        optionalUsuarios = Optional.of(new Usuarios(ID, NOME, CPF, EMAIL, SENHA, CLIENTE));
     }
 }
